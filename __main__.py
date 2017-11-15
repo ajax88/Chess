f3295a49ccb380f7a2307647e765b76806b9fc22
khoskins <katiehoskins18@gmail.com>
2017-11-14 21:24:13 -0800

    Updated some of the messages/game appearance. Began game logic of turn switching.

--- __main__.py
+++ __main__.py
@@ -1,18 +1,36 @@
 from sample.game.chess_game import ChessGame
+import os
 
 
 def main():
     while True:
+        os.system('clear')
+        print_title()
         response = input("Would you like to flip the board (y/n)")
-        if response.lower().__eq__("y"):
+        if response.lower() == "y":
             flip_board = True
             break
-        if response.lower().eq("n"):
+        if response.lower() == "n":
             flip_board = False
             break
 
     game = ChessGame(flip_board)
     game.start()
+
+def print_title():
+    print("  /$$$$$$  /$$                                   ")
+    print(" /$$__  $$| $$                                   ")
+    print("| $$  \__/| $$$$$$$   /$$$$$$   /$$$$$$$ /$$$$$$$")
+    print("| $$      | $$__  $$ /$$__  $$ /$$_____//$$_____/")
+    print("| $$      | $$  \ $$| $$$$$$$$|  $$$$$$|  $$$$$$ ")
+    print("| $$    $$| $$  | $$| $$_____/ \____  $$\____  $$")
+    print("|  $$$$$$/| $$  | $$|  $$$$$$$ /$$$$$$$//$$$$$$$/")
+    print(" \______/ |__/  |__/ \_______/|_______/|_______/ ")
+    print("\n")
+    print("             Alex Jackson & Katie Hoskins        ")
+    print("                    Fall 2017                    ")
+    print("\n")
+    print("\n")
 
 
 if __name__ == "__main__":
--- sample/game/chess_game.py
+++ sample/game/chess_game.py
@@ -3,6 +3,8 @@ from sample.board.chess_board import ChessBoard
 from sample.player.chess_player import ChessPlayer
 from sample.helpers import constants
 
+import os
+
 
 class ChessGame(Game):
     def __init__(self, flip_board):
@@ -12,25 +14,36 @@ class ChessGame(Game):
 
     def start(self):
         self.init_players()
-
+        self.play_game()
 
     def print_board(self):
         print(self.board)
 
+    def change_current_player (self) :
+        if self.current_player == self.player1 :
+            self.current_player = self.player2
+        else :
+            self.current_player = self.player1
+
+
     def init_players(self):
-        p1_name = input("Enter player 1 name")
-        p2_name = input("Enter player 2 name")
+        p1_name = input("Enter player 1 name : ")
+        p2_name = input("Enter player 2 name : ")
         self.player1.set_name(p1_name)
         self.player2.set_name(p2_name)
         while True:
             try:
-                p1_color = input("Enter player 1 color")
+                p1_color = input(p1_name + ", do you want black or white? ")
                 self.player1.set_color(p1_color)
                 break
             except ValueError:
                 print("Color must be either black or white.")
 
         self.player2.set_color(constants.get_other_color(self.player1.get_color()))
+        if (self.player1.color == "white") : 
+            self.current_player = self.player1
+        else:
+            self.current_player = self.player2
 
     def get_player_1(self):
         return self.player1
@@ -41,6 +54,17 @@ class ChessGame(Game):
     def parse_move(self, move, color):
         move = move.lower()
         piece_name = move[0]
+
+    def play_game(self):
+        game_over = False
+        while(not game_over):
+            os.system('clear')
+            print(self.board)
+            print(self.current_player.get_name() + ", it's your turn. ")
+            move_string = input("Move: ")
+            if (move_string == "quit") :
+                game_over = True
+            self.change_current_player()
 
 
 
