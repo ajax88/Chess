abba079160c1e1432d2a33aa19424614be672ea9
Alex Jackson <ajjackson91195@gmail.com>
2018-01-17 21:00:16 -0800

    Add todo

--- sample/chesspiece/king.py
+++ sample/chesspiece/king.py
@@ -17,8 +17,10 @@ class King(ChessPiece):
         if abs(to_col - self.col) == 2 and to_row == self.row: # castle on either side
             if self.has_moved:
                 return False
-            if self.board.is_blocked(self.row, self.col, to_row, to_col):
+            if self.board.is_blocked(self.row, self.col, to_row, to_col): # TODO: Check if rook is blocked... prevents fringe case where an enemy piece is in the way of the check
                 return False
+            ## TODO: get rook, check if it's blocked. See if king would be in check with both moves that it would make
+            ## TODO: if valid, switch em up
 
         if abs(to_col - self.col) == 1 or abs(to_row - self.row) == 1:
             curr_spot = self.board.get_square(to_row, to_col)
